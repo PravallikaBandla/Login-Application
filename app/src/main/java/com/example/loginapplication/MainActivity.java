755c5c2fe6d55209617834e10b2e47e6c0629a80
pravallikab <pravallikab.in@mouritech.com>
2021-03-22 20:52:08 +0530

    .

--- app/src/main/java/com/example/loginapplication/MainActivity.java
+++ app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.loginapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+method();
+    }
+    public void method()
+    {
+        Toast.makeText(getApplicationContext(),"pravallika", Toast.LENGTH_SHORT).show();
+
     }
 }
