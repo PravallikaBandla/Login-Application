15d71b243d97fd04871b6a1f0b8c2f7551c797bc
pravallikab <pravallikab.in@mouritech.com>
2021-03-22 19:28:39 +0530

    Initial Commit

---
+++ myloginlibrary/src/main/java/com/example/myloginlibrary/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.myloginlibrary;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Pravallika on 22-03-2021.
+ * Updated by Pravallika on 22-03-2021.
+ * <p>
+ * <p>
+ * <p>
+ * Copyright (c) 2021 MouriTech. All rights reserved.
+ */
+class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"pravallika", Toast.LENGTH_SHORT).show();
+
+    }
+}
