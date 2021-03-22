1fbe5b771552c52957dd803b048051fd87f449fe
pravallikab <pravallikab.in@mouritech.com>
2021-03-22 21:31:52 +0530

    .

--- app/src/main/java/com/example/loginapplication/MainActivity.java
+++ app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -5,17 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.myloginlibrary.LoginActivityy;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-method();
+       // LoginActivityy.method();
     }
-    public void method()
-    {
-        Toast.makeText(getApplicationContext(),"pravallika", Toast.LENGTH_SHORT).show();
-
-    }
+//    public void method()
+//    {
+//        Toast.makeText(getApplicationContext(),"pravallika", Toast.LENGTH_SHORT).show();
+//
+//    }
 }
---
+++ myloginlibrary/src/main/java/com/example/myloginlibrary/LoginActivityy.java
@@ -0,0 +1,16 @@
+package com.example.myloginlibrary;
+
+/**
+ * Created by Pravallika on 22-03-2021.
+ * Updated by Pravallika on 22-03-2021.
+ * <p>
+ * <p>
+ * <p>
+ * Copyright (c) 2021 MouriTech. All rights reserved.
+ */
+public class LoginActivityy {
+    public static int method(int a, int b)
+    {
+        return a+b;
+    }
+}
